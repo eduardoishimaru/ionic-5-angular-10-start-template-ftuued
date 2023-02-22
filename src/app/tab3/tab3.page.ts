58651543111e7d5d8db61aa0f1e5cd30535a6576
eduardoishimaru <eduardoishimaru@gmail.com>
2023-02-22 15:34:14 -0300

    3 files modified

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
+  form: FormGroup;
 
-  constructor() {}
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private contatoService: ContatoService,
+    public toastController: ToastController
+  ) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
+
   whats(){
     this.abrirUrl('https://api.whatsapp.com/send?phone=5511963040072&text=Ol%C3%A1%20vim%20atraves%20do%20seu%20App%20Gostaria%20de%20receber%20doacoes');
    
@@ -17,4 +32,8 @@ export class Tab3Page {
     window.open(url,"_blank").focus();
 
   }
+  enviar(){
+    console.log('Enviado')
+  }
+
 }
